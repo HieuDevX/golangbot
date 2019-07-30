7484f73abb610045e1937856dab6cfe9a7dce7f1
hieuht <hoangtrunghieu10licbn@gmail.com>
2019-07-30 18:13:16 +0700

    packages

--- packages/geometry/geometry.go
+++ packages/geometry/geometry.go
@@ -2,12 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
+	_ "log"
 
 	"github.com/hieudevx/golangbot-tms/packages/geometry/rectangle"
 )
 
+/*
+ * 1. Package variables
+ */
+
+var rectLen, rectWidth float64 = 6, 7
+
+/*
+ * 2. init func to validate if the length and width less than 0
+ */
+
+func init() {
+	println("main package intiialized")
+	if rectLen < 0 {
+		log.Fatal("length is less than zero")
+	}
+	if rectWidth < 0 {
+		log.Fatal("width is less than zero")
+	}
+}
+
 func main() {
-	var rectLen, rectWidth float64 = 6, 7
+	// var rectLen, rectWidth float64 = 6, 7
 	fmt.Println("Geometrical shape properties")
 
 	fmt.Printf("Area of rectangle %.2f \n", rectangle.Area(rectLen, rectWidth))
--- packages/geometry/rectangle/rectprops.go
+++ packages/geometry/rectangle/rectprops.go
@@ -1,12 +1,24 @@
 package rectangle
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
+/*
+ * init func
+ */
+func init() {
+	fmt.Println("rectangle package initialized")
+}
+
+// Area func
 func Area(len, wid float64) float64 {
 	area := len * wid
 	return area
 }
 
+// Diagonal func
 func Diagonal(len, wid float64) float64 {
 	diagonal := math.Sqrt((len * len) + (wid * wid))
 	return diagonal
